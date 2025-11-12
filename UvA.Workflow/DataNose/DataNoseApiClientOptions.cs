6a3776fa4b863b70e8efbebbbcdc6fb131519585
Robert Boer <r.boer@uva.nl>
2025-11-12 12:36:50 +0100

    DN-3384 Fix build error

--- UvA.Workflow/DataNose/DataNoseApiClientOptions.cs
+++ UvA.Workflow/DataNose/DataNoseApiClientOptions.cs
@@ -3,6 +3,6 @@ namespace UvA.Workflow.DataNose;
 public class DataNoseApiClientOptions
 {
     public static string Section = "DataNoseApi";
-    public string BaseAddress { get; set; }
-    public string ApiKey { get; set; }
+    public string BaseAddress { get; set; } = null!;
+    public string ApiKey { get; set; } = null!;
 }
