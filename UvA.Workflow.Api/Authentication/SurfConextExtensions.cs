f2022bca406a95bd7c4b11fc5284272cb31dfa42
robo <r.boer@uva.nl>
2025-11-16 19:18:45 +0100

    DN-3382 feate:SURFconext authentication (#19)
    * DN-3382 feate:SURFconext authentication
    
    * Updates following feedback in PR
    
    * Small fix for claim mapping
    
    * Explicitly log missing config values
    
    * Fxied casing in helm chart
    
    * fix RP
    
    * Revert "fix RP"
    
    This reverts commit 4d202d853e46ca726e495899554031a0ca00fd76.
    
    * Fix client id
    
    * Fxi clientid
    
    * REset client id
    
    * Added authority
    
    * Added logging of surfconet config
    
    * Fixed casing
    
    * Removed some loggnig and disabled controller wide auth for now
    
    * DN-3382 Enable authentication
    
    * DN-3382 chore: formatting
    
    * DN-3382 chore: more formatting
    
    * DN-3382 fix: typo
    
    ---------
    
    Co-authored-by: Gerrit Oomens <g.oomens@uva.nl>

---
+++ UvA.Workflow.Api/Authentication/SurfConextExtensions.cs
@@ -0,0 +1,102 @@
+using System.Text;
+using Microsoft.OpenApi.Models;
+using UvA.Workflow.Api.Infrastructure;
+
+namespace UvA.Workflow.Api.Authentication;
+
+public static class SurfConextExtensions
+{
+    public static IServiceCollection AddSurfConextAuthentication(this IServiceCollection services,
+        IWebHostEnvironment environment, IConfiguration config)
+    {
+        services.AddMemoryCache();
+
+        var options = config.GetSection(SurfConextOptions.Section).Get<SurfConextOptions>();
+
+        if (string.IsNullOrEmpty(options?.BaseUrl))
+            throw new InvalidOperationException("Missing SurfConextOptions.BaseUrl");
+        if (string.IsNullOrEmpty(options.ClientId))
+            throw new InvalidOperationException("Missing SurfConextOptions.ClientId");
+        if (string.IsNullOrEmpty(options.ClientSecret))
+            throw new InvalidOperationException("Missing SurfConextOptions.ClientSecret");
+
+        services.Configure<SurfConextOptions>(config.GetSection(SurfConextOptions.Section));
+
+        services.AddHttpClient<SurfConextAuthenticationHandler>(client =>
+        {
+            client.BaseAddress = new Uri(options.BaseUrl!);
+            client.DefaultRequestHeaders.Add("Authorization",
+                $"Basic {Convert.ToBase64String(Encoding.ASCII.GetBytes($"{options.ClientId}:{options.ClientSecret}"))}");
+        });
+
+        services.AddAuthentication(authOptions =>
+        {
+            authOptions.AddScheme<SurfConextAuthenticationHandler>(SurfConextAuthenticationHandler.Scheme, null);
+        });
+
+        services.AddSwaggerGen(c =>
+        {
+            if (environment.IsDevOrTest()) // OIDC (SURFconext) authentication
+            {
+                c.AddSecurityDefinition("OIDC",
+                    new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        BearerFormat = "JWT",
+                        Scheme = "Bearer",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.OAuth2,
+                        Flows = new OpenApiOAuthFlows
+                        {
+                            AuthorizationCode = new OpenApiOAuthFlow
+                            {
+                                AuthorizationUrl = new Uri("https://auth-pr.datanose.nl/auth"),
+                                TokenUrl = new Uri("https://auth-pr.datanose.nl/token")
+                            }
+                        }
+                    });
+            }
+
+            // Bearer token authentication
+            c.AddSecurityDefinition("Bearer",
+                new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer",
+                    Description = "Specify the authorization token",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                });
+
+            //New code to work with .NET6
+            var securityRequirement = new OpenApiSecurityRequirement();
+            if (environment.IsDevOrTest())
+            {
+                securityRequirement.Add(new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "OIDC"
+                        }
+                    },
+                    []);
+            }
+
+            securityRequirement.Add(new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                []);
+
+            c.AddSecurityRequirement(securityRequirement);
+        });
+
+        return services;
+    }
+}
