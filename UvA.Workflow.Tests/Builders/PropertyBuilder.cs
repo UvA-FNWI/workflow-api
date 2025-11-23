f1b883a900023c61a17520f735ed3162543e43d8
robo <r.boer@uva.nl>
2025-11-23 10:09:06 +0100

    Added initial unit test plus builders (#21)
    * Added initial unit test plus builders
    
    * Copied… I mean, ‘collaborated’ on expression and template tests.
    
    ---------
    
    Co-authored-by: Gerrit Oomens <g.oomens@uva.nl>

---
+++ UvA.Workflow.Tests/Builders/PropertyBuilder.cs
@@ -0,0 +1,71 @@
+using Bogus;
+using MongoDB.Bson;
+
+namespace UvA.Workflow.Tests;
+
+public class PropertyBuilder
+{
+    private readonly Faker faker = new();
+
+    public BsonValue Value(BsonValue value) => value;
+
+    public BsonDocument Person(string? displayName = null, string? email = null, string? externalId = null,
+        string? objectId = null)
+        => new()
+        {
+            { "_id", NewObjectId(objectId) },
+            { "ExternalId", externalId ?? faker.Random.Int().ToString() },
+            { "DisplayName", displayName ?? faker.Name.FullName() },
+            { "Email", email ?? faker.Internet.Email() }
+        };
+
+    public BsonDocument FileRef(string? name = null, string? objectId = null)
+        => new()
+        {
+            { "_id", NewObjectId(objectId) },
+            { "Name", name ?? faker.Lorem.Word() }
+        };
+
+    public BsonArray Array(params BsonValue[] items) => new(items);
+
+    public BsonArray Array(params Func<PropertyBuilder, BsonValue>[] items)
+    {
+        var array = new BsonArray();
+        foreach (var builder in items)
+        {
+            var propertyBuilder = new PropertyBuilder();
+            array.Add(builder(propertyBuilder));
+        }
+
+        return array;
+    }
+
+    public BsonArray Array(int size, Func<PropertyBuilder, int, BsonValue> builder)
+    {
+        var array = new BsonArray();
+        for (var i = 0; i < size; i++)
+        {
+            var propertyBuilder = new PropertyBuilder();
+            array.Add(builder(propertyBuilder, i));
+        }
+
+        return array;
+    }
+
+    public BsonArray Array(int size, Func<PropertyBuilder, BsonValue> builder)
+    {
+        var array = new BsonArray();
+        for (var i = 0; i < size; i++)
+        {
+            var propertyBuilder = new PropertyBuilder();
+            array.Add(builder(propertyBuilder));
+        }
+
+        return array;
+    }
+
+    private ObjectId NewObjectId(string? objectId)
+    {
+        return string.IsNullOrEmpty(objectId) ? ObjectId.GenerateNewId() : new ObjectId(objectId);
+    }
+}
--- UvA.Workflow/Submissions/AnswerService.cs
+++ UvA.Workflow/Submissions/AnswerService.cs
@@ -12,7 +12,7 @@ public class AnswerService(
     ModelService modelService,
     InstanceService instanceService,
     RightsService rightsService,
-    ArtifactService artifactService,
+    IArtifactService artifactService,
     AnswerConversionService answerConversionService)
 {
     public async Task<QuestionContext> GetQuestionContext(
