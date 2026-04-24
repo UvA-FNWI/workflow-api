c0e50d3b7d14a3836eeca14b81461e5854f8844d
Michel Roos <m.roos3@uva.nl>
2026-04-24 16:31:51 +0200

    DN-3667 (feat) Updated ScreensControllerTests unit test with actual data

--- UvA.Workflow.Tests/Controllers/ScreensControllerTests.cs
+++ UvA.Workflow.Tests/Controllers/ScreensControllerTests.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Moq;
 using UvA.Workflow.Api.Screens;
 using UvA.Workflow.Api.Screens.Dtos;
 using UvA.Workflow.Tests.Controllers.Helpers;
+using UvA.Workflow.WorkflowInstances;
 
 namespace UvA.Workflow.Tests.Controllers;
 
@@ -16,44 +19,68 @@ public class ScreensControllerTests : ControllerTestsBase
                 _workflowInstanceRepoMock.Object));
     }
 
-    [Theory]
-    [InlineData("Student")]
-    [InlineData("Admin")]
-    [InlineData("UnknownRole")]
-    public async Task Screens_GetScreenData_AllowAnyone(string role)
+    [Fact]
+    public async Task Screens_GetScreenData_ReturnsData()
     {
         // Arrange
         const string workflowDefinition = "Project";
         const string screenName = "Projects";
 
-        var controller = BuildControllerWithRoles([role]);
+        var controller = BuildControllerWithRoles(["Student"], workflowDefinition, screenName);
+
         // Act
         var result = await controller.GetScreenData(workflowDefinition, screenName, _ct);
+
         //Assert
-        Assert.IsType<ActionResult<ScreenDataDto>>(result);
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var screenDataDto = Assert.IsType<ScreenDataDto>(okResult.Value);
+        Assert.Equal(screenName, screenDataDto.Name);
+        Assert.Equal(workflowDefinition, screenDataDto.WorkflowDefinition);
+        Assert.Equal(2, screenDataDto.Rows.Length);
     }
 
-    [Theory]
-    [InlineData("Student")]
-    [InlineData("Admin")]
-    [InlineData("UnknownRole")]
-    public async Task Screens_GetGroupedScreenData_AllowAnyone(string role)
+    [Fact]
+    public async Task Screens_GetGroupedScreenData_ReturnsGroupedData()
     {
         // Arrange
         const string workflowDefinition = "Project";
         const string screenName = "Projects";
 
-        var controller = BuildControllerWithRoles([role]);
+        var controller = BuildControllerWithRoles(["Student"], workflowDefinition, screenName);
         // Act
-        var result = await controller.GetGroupedScreenData(workflowDefinition, screenName, _ct);
+        var result = await controller.GetGroupedScreenData(screenName, workflowDefinition, _ct);
+
         //Assert
-        Assert.IsType<ActionResult<GroupedScreenDataDto>>(result);
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var screenDataDto = Assert.IsType<GroupedScreenDataDto>(okResult.Value);
+        Assert.Equal(screenName, screenDataDto.Name);
+        Assert.Equal(3, screenDataDto.Groups.Length);
+        Assert.Contains(screenDataDto.Groups, g => g.Name == "assign-subject");
+        Assert.Contains(screenDataDto.Groups, g => g.Name == "thesis-in-progress");
+        Assert.Contains(screenDataDto.Groups, g => g.Name == "completed");
     }
 
     private ScreensController BuildControllerWithRoles(
-        string[] roles)
+        string[] roles, string workflowDefinition, string screenName = "Projects")
     {
         MockCurrentUser(roles);
+        MockEmptyRelatedInstanceLookups();
+
+        _workflowInstanceRepoMock.Setup(r => r.GetAllByType(workflowDefinition,
+                It.IsAny<Dictionary<string, string>>(),
+                It.IsAny<BsonDocument?>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync([
+                new Dictionary<string, BsonValue>()
+                {
+                    { "CurrentStep", "Start" }
+                },
+                new Dictionary<string, BsonValue>()
+                {
+                    { "CurrentStep", "Start" }
+                }
+            ]);
+
         return new ScreensController(_screenDataService);
     }
 }
