bdc23cd46658792b7ad2555908ab666bb6a4bd8c
Michel Roos <m.roos3@uva.nl>
2026-04-24 15:03:20 +0200

    DN-3667 (chore) updated SubmissionsControllerTests with actual form data and verifications

--- UvA.Workflow.Tests/Controllers/SubmissionsControllerTests.cs
+++ UvA.Workflow.Tests/Controllers/SubmissionsControllerTests.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
 using Moq;
 using UvA.Workflow.Api.Infrastructure;
 using UvA.Workflow.Api.Submissions;
@@ -28,9 +30,15 @@ public class SubmissionsControllerTests : ControllerTestsBase
         _submissionDtoFactory =
             new SubmissionDtoFactory(new ArtifactTokenService(_configurationMock.Object), _modelService);
         _workflowInstanceDtoFactory =
-            new WorkflowInstanceDtoFactory(_instanceService, _modelService,
-                _submissionDtoFactory, _workflowInstanceRepoMock.Object, _rightsService,
-                new StepVersionService(_modelService, _eventRepoMock.Object), _workflowInstanceService,
+            new WorkflowInstanceDtoFactory(
+                _instanceService,
+                _modelService,
+                _submissionDtoFactory,
+                _workflowInstanceRepoMock.Object,
+                _rightsService,
+                new StepVersionService(_modelService, _eventRepoMock.Object),
+                new StepHeaderStatusResolver(_modelService),
+                _workflowInstanceService,
                 _loggerFactory.CreateLogger<WorkflowInstanceDtoFactory>());
     }
 
@@ -46,22 +54,69 @@ public class SubmissionsControllerTests : ControllerTestsBase
         var result = await controller.GetSubmission(instance.Id, submissionId, null, _ct);
 
         //Assert
-        Assert.IsType<ActionResult<SubmissionDto>>(result);
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var payload = Assert.IsType<SubmissionDto>(okResult.Value);
+        Assert.Equal(submissionId, payload.Id);
+        Assert.Equal(instance.Id, payload.InstanceId);
+        Assert.Equal(submissionId, payload.FormName);
+        Assert.NotNull(payload.Form);
+        Assert.NotNull(payload.Answers);
     }
 
-    [Theory]
-    [InlineData("Student")]
-    public async Task Submissions_SubmitSubmission_AllowWithSubmitRights(string role)
+    [Fact]
+    public async Task Submissions_SubmitSubmission_DeniedOnInvalidForm()
     {
         // Arrange
         const string submissionId = "Start";
-        var (controller, instance) = BuildControllerWithRoles([role], submissionId);
+        var (controller, instance) = BuildControllerWithRoles(["Student"], submissionId);
 
         // Act
         var result = await controller.SubmitSubmission(instance.Id, submissionId, _ct);
 
         //Assert
-        Assert.IsType<ActionResult<SubmitSubmissionResult>>(result);
+        var actionResult = Assert.IsType<ActionResult<SubmitSubmissionResult>>(result);
+        var unprocessableResult = Assert.IsType<UnprocessableEntityObjectResult>(actionResult.Result);
+
+        Assert.Equal(StatusCodes.Status422UnprocessableEntity, unprocessableResult.StatusCode);
+    }
+
+    [Fact]
+    public async Task Submissions_SubmitSubmission_AllowedWithValidForm()
+    {
+        // Arrange
+        const string submissionId = "Start";
+
+        // Supply the instance with a full list of properties
+        var (controller, instance) = BuildControllerWithRoles(["Student"], submissionId, "Start",
+            ("Title", _ => "Title"),
+            ("Subject", _ => "Subject"),
+            ("Description", _ => new BsonDocument
+            {
+                { "Name", "Name" }
+            }),
+            ("Examiner", _ => new BsonDocument()),
+            ("Reviewer", _ => new BsonDocument()),
+            ("Supervisor", _ => new BsonDocument()),
+            ("StartDate", _ => new DateTime(2056, 01, 01, 9, 0, 0, DateTimeKind.Utc)),
+            ("EndDate", _ => new DateTime(2057, 01, 01, 9, 0, 0, DateTimeKind.Utc)),
+            ("Deadline", _ => new DateTime(2058, 01, 01, 9, 0, 0, DateTimeKind.Utc)),
+            ("EC", _ => 1)
+        );
+
+        // Act
+        var result = await controller.SubmitSubmission(instance.Id, submissionId, _ct);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var submissionResult = Assert.IsType<SubmitSubmissionResult>(okResult.Value);
+
+        Assert.True(submissionResult.Success);
+        Assert.True(submissionResult.Success);
+        Assert.NotNull(submissionResult.Submission);
+        Assert.Equal(submissionId, submissionResult.Submission.Id);
+        Assert.Equal(instance.Id, submissionResult.Submission.InstanceId);
+        Assert.NotNull(submissionResult.UpdatedInstance);
+        Assert.Null(submissionResult.ValidationErrors);
     }
 
     [Theory]
@@ -83,6 +138,7 @@ public class SubmissionsControllerTests : ControllerTestsBase
     {
         const string submissionId = "Start";
         var (controller, instance) = BuildControllerWithRoles(["Student"], submissionId);
+
         _userServiceMock.Setup(s => s.GetCurrentUser(It.IsAny<CancellationToken>()))
             .ReturnsAsync((User?)null);
 
@@ -94,15 +150,31 @@ public class SubmissionsControllerTests : ControllerTestsBase
     }
 
     private (SubmissionsController Controller, WorkflowInstance Instance) BuildControllerWithRoles(
-        string[] roles, string submissionId, string stepName = "Start")
+        string[] roles, string submissionId, string stepName = "Start",
+        params (string name, Func<PropertyBuilder, BsonValue> builder)[] props)
     {
+        var courseInstance = new WorkflowInstanceBuilder()
+            .With("Context", "Start")
+            .WithProperties(
+                ("Name", _ => "CourseTitle"),
+                ("ExternalId", _ => "DummyId"),
+                ("Type", _ => "Course"),
+                ("Coordinator", _ => new BsonDocument()))
+            .Build();
+
+        MockInstance(courseInstance);
+
         var instance = new WorkflowInstanceBuilder()
             .With(workflowDefinition: "Project", currentStep: stepName)
             .WithEvents(b => b.WithId(submissionId))
+            .WithProperties(("Course", _ => courseInstance.Id))
+            .WithProperties(props)
             .Build();
 
         MockInstance(instance);
+        MockEmptyEventLog(instance);
         MockCurrentUser(roles);
+        MockEmptyRelatedInstanceLookups();
 
         var controller = new SubmissionsController(_userServiceMock.Object, _modelService, _rightsService,
             _submissionService, _submissionDtoFactory, _workflowInstanceDtoFactory);
