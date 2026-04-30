880bd4ec835f8852eee6f882c32db8e9565ae4cc
Annes Negmel-Din <annesnour03@gmail.com>
2026-04-30 14:18:58 +0200

    DN-3670 refactor: remove invite controller

--- UvA.Workflow.Users.EduId/InviteUserRequest.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace UvA.Workflow.Users.EduId;
-
-public record InviteUserRequest(
-    [Required] [EmailAddress] string Email,
-    [Required] [MinLength(1)] string UserName
-);
--- UvA.Workflow.Users.EduId/InviteUserResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace UvA.Workflow.Users.EduId;
-
-public record InviteUserResponse(
-    string UserId,
-    string Email,
-    string UserName,
-    string InvitationUrl);
