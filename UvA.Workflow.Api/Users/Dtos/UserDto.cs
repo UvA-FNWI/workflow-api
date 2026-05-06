9197284a34f7af74b18763f17a0896a46cfc03bd
annes <76728727+annesnour03@users.noreply.github.com>
2026-05-06 15:59:42 +0200

    DN-3745 feat: add preferred language to user (#129)
    * DN-3745 feat: add preferred language to user
    
    * DN-3745 fix: correct variables in builder
    
    * DN-3745 fix: ignore extra elements on instanceuser

--- UvA.Workflow.Api/Users/Dtos/UserDto.cs
+++ UvA.Workflow.Api/Users/Dtos/UserDto.cs
@@ -7,7 +7,8 @@ public record UserDto(
     string Id,
     string UserName,
     string DisplayName,
-    string Email
+    string Email,
+    string? PreferredLanguage
 )
 {
     /// <summary>
@@ -19,7 +20,8 @@ public record UserDto(
             user.Id,
             user.UserName,
             user.DisplayName,
-            user.Email
+            user.Email,
+            user.PreferredLanguage
         );
     }
 }
