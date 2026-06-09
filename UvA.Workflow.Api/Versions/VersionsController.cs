a49889810da2e640cf46b4beb07052b78349235a
Annes Negmel-Din <annesnour03@gmail.com>
2026-06-09 10:03:28 +0200

    fix: remove InvitesController and disable versions controller

--- UvA.Workflow.Api/Versions/VersionsController.cs
+++ UvA.Workflow.Api/Versions/VersionsController.cs
@@ -3,30 +3,30 @@ using UvA.Workflow.Api.Infrastructure;
 using UvA.Workflow.WorkflowModel;
 
 namespace UvA.Workflow.Api.Versions;
-
-[ApiExplorerSettings(IgnoreApi = true)]
-[AllowAnonymous]
-public class VersionsController(ModelServiceResolver modelServiceResolver, ILogger<VersionsController> logger)
-    : ApiControllerBase
-{
-    [HttpPost("{version}")]
-    public ActionResult CreateVersion(string version, [FromBody] Dictionary<string, string> files)
-    {
-        ModelParser parser;
-        try
-        {
-            parser = new ModelParser(new DictionaryProvider(files));
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Error parsing model for version {Version}", version);
-            return BadRequest(ex.Message);
-        }
-
-        modelServiceResolver.AddOrUpdate(version, parser);
-        return Ok();
-    }
-
-    [HttpGet]
-    public ActionResult<IEnumerable<string>> GetVersions() => Ok(modelServiceResolver.Versions);
-}
+// TODO: add back when we have correct auth for this
+// [ApiExplorerSettings(IgnoreApi = true)]
+// [AllowAnonymous]
+// public class VersionsController(ModelServiceResolver modelServiceResolver, ILogger<VersionsController> logger)
+//     : ApiControllerBase
+// {
+//     [HttpPost("{version}")]
+//     public ActionResult CreateVersion(string version, [FromBody] Dictionary<string, string> files)
+//     {
+//         ModelParser parser;
+//         try
+//         {
+//             parser = new ModelParser(new DictionaryProvider(files));
+//         }
+//         catch (Exception ex)
+//         {
+//             logger.LogError(ex, "Error parsing model for version {Version}", version);
+//             return BadRequest(ex.Message);
+//         }
+//
+//         modelServiceResolver.AddOrUpdate(version, parser);
+//         return Ok();
+//     }
+//
+//     [HttpGet]
+//     public ActionResult<IEnumerable<string>> GetVersions() => Ok(modelServiceResolver.Versions);
+// }
