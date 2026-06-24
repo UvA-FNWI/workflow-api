2019e1ad9e7f824e5afc1081601df6688c590d54
Gerrit Oomens <g.oomens@uva.nl>
2026-06-24 14:49:03 +0200

    DN-3783 fix: better error on invalid type

--- UvA.Workflow/WorkflowModel/ModelParser.cs
+++ UvA.Workflow/WorkflowModel/ModelParser.cs
@@ -328,6 +328,15 @@ public partial class ModelParser
 
     private PropertyDefinition PreProcess(PropertyDefinition propertyDefinition)
     {
+        try
+        {
+            _ = propertyDefinition.DataType;
+        }
+        catch (Exception)
+        {
+            throw new Exception($"Invalid data type {propertyDefinition.Type} for property {propertyDefinition.Name}");
+        }
+
         propertyDefinition.Layout = NormalizeLayout(propertyDefinition.Layout);
 
         foreach (var entry in propertyDefinition.Values ?? [])
