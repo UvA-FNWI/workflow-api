7daebfd51801b4156dd1063db5c93d2097b13d2c
annes <76728727+annesnour03@users.noreply.github.com>
2026-08-18 19:31:56 +0200

    DN-3957 feat: add schema check (#236)
    * DN-3957 feat: add schema check
    
    * DN-3957 fix: schema's update

--- UvA.Workflow.SchemaGenerator/Worker.cs
+++ UvA.Workflow.SchemaGenerator/Worker.cs
@@ -21,12 +21,23 @@ public class Worker(IHostApplicationLifetime lifetime) : BackgroundService
             typeof(ValueSet)
         ];
 
+        var schemasChanged = false;
         foreach (var type in types)
         {
             var schema = generator.Generate(type).ToJson();
-            await File.WriteAllTextAsync($"../Schemas/{type.Name}.json", schema, stoppingToken);
+            schemasChanged |= await WriteSchema($"../Schemas/{type.Name}.json", schema, stoppingToken);
         }
 
+        Environment.ExitCode = schemasChanged ? 1 : 0;
         lifetime.StopApplication();
+    }
+
+    public static async Task<bool> WriteSchema(string path, string schema, CancellationToken cancellationToken)
+    {
+        if (File.Exists(path) && await File.ReadAllTextAsync(path, cancellationToken) == schema)
+            return false;
+
+        await File.WriteAllTextAsync(path, schema, cancellationToken);
+        return true;
     }
 }
