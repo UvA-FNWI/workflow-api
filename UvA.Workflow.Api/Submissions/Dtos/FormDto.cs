9164690ed37f1135bf41ed9921d65ee7f5ff9e2e
Danielle Petersen <d.j.kwakkel@uva.nl>
2026-09-24 16:21:56 +0200

    DN-3972: Pilot Political Science (#257)
    * DN-3972 fix: Allow comparison of conditions in Double type fields
    
    * DN-3972 feat: Update schemas
    
    * DN-3972 feat: Add minLength condition to string properties
    
    * DN-3972 feat: Updates schemas

--- UvA.Workflow.Api/Submissions/Dtos/FormDto.cs
+++ UvA.Workflow.Api/Submissions/Dtos/FormDto.cs
@@ -133,6 +133,7 @@ public record QuestionDto(
     decimal? Weight,
     decimal? Percentage,
     int? MaxLength,
+    int? MinLength,
     bool? AllowsExternalUsers,
     List<RubricEntryDto>? Rubric,
     ValueSetSorting? Sorting,
@@ -166,6 +167,8 @@ public record QuestionDto(
             .Select(entry => RubricEntryDto.Create(entry, propertyDefinition.Values))
             .ToList();
 
+        var validationValues = propertyDefinition.Validation.GetAllValues().ToArray();
+
         return new QuestionDto(
             Id: $"{propertyDefinition.ParentType.Name}_{propertyDefinition.Name}",
             Name: propertyDefinition.Name,
@@ -182,7 +185,8 @@ public record QuestionDto(
             HideInResults: propertyDefinition.HideInResults,
             Weight: weight,
             Percentage: percentage,
-            MaxLength: propertyDefinition.Validation?.Value?.MaxLength,
+            MaxLength: validationValues.Select(v => v.MaxLength).FirstOrDefault(v => v != null),
+            MinLength: validationValues.Select(v => v.MinLength).FirstOrDefault(v => v != null),
             AllowsExternalUsers: propertyDefinition.AllowsExternalUsers,
             Rubric: rubric,
             Sorting: propertyDefinition.Sorting,
